8f7f96949d9164f3011c49c5ea7ec7a9a7b77047
gufran <mohammadgufran.abdullah5152@gmail.com>
2023-12-23 12:47:56 +0500

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,6 +1,14 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
+dotenv.config();
+mongoose.connect(process.env.MONGO_STRING).then(()=>{
+    console.log("Connected to MongoDB!!!");
+}).catch((error) => {
+    console.log(error);
+});
 const app = express();
-app.listen(3000, () => {
-    console.log("Server is running on port 3000");
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on port ${process.env.PORT}`);
 });
